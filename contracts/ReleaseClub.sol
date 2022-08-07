24a9e201b86263d8ceb78d09877c7179fc9d5221
dhruv035 <dhruv035@gmail.com>
2022-08-07 10:01:46 +0530

    members

--- contracts/ReleaseClub.sol
+++ contracts/ReleaseClub.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-struct Release 
-{
+struct Release {
     address tokenContract;
     address token;
 }
@@ -14,19 +13,29 @@ contract ReleaseClub is AccessControl{
    
    Release[] public releases;
    bytes32 public constant MEMBER_ROLE = keccak256("MEMBER_ROLE");
+   bytes32 public constant MOD_ROLE = keccak256("MOD_ROLE");
     constructor() {
         // Grant the contract deployer the default admin role: it will be able
         // to grant and revoke any roles
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MOD_ROLE,msg.sender);
     }
-   function addMember(address account) external onlyRole (DEFAULT_ADMIN_ROLE) {
+   function addMember(address account) external onlyRole (MOD_ROLE) {
        _grantRole(MEMBER_ROLE,account);
    }
-   function revokeMember(address account) external onlyRole (DEFAULT_ADMIN_ROLE) {
+   function revokeMember(address account) external onlyRole (MOD_ROLE) {
        _revokeRole(MEMBER_ROLE,account);
    }
-   function addRelease(address tokenContract,address token) public onlyRole (MEMBER_ROLE) {
-       Release memory newRelease = Release(tokenContract,token);
-       releases.push(newRelease);
+   function addModerator(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+       _grantRole(MOD_ROLE, account);
    }
+   function addRelease(Release[] memory newReleases, uint256 length) public onlyRole (MEMBER_ROLE) {
+       uint256 i = 0;
+       while(i<length)
+       {
+           releases.push(newReleases[i]);
+           i++;
+       }
+   }
+
 }
