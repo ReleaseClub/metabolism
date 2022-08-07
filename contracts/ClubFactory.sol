271a56205daaae185fe672a978cf7d14df4ff5fb
dhruv035 <dhruv035@gmail.com>
2022-08-07 21:39:24 +0530

    add function

--- contracts/ClubFactory.sol
+++ contracts/ClubFactory.sol
@@ -8,15 +8,22 @@ contract ClubFactory is Ownable {
     event ClubCreated(address ClubAddress, string clubName);
 
     address[] public clubs;
+    mapping(address=>address[]) public clubOwners;
 
-    function addClub(string calldata name) public payable {
+    function addClub(string memory name) public payable {
         ReleaseClub club = new ReleaseClub(name,msg.sender);
         clubs.push(address(club));
+        clubOwners[msg.sender].push(address(club));
         emit ClubCreated(address(club),name);
+        
         
     }
 
     function viewClubs() public view returns(address[] memory){
         return clubs;
+    }
+
+    function getClubs(address owner) public view returns (address[] memory) {
+        return clubOwners[owner];
     }
 }
